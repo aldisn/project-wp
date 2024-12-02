005a8fa383781b2d6edcc483a918a0506004ef32
Vanquisher-sketch <m.ivantaufiqap@gmail.com>
2024-12-02 08:08:06 +0700

    siuu

--- project--wp/app/Http/Controllers/HomeController.php
+++ project--wp/app/Http/Controllers/HomeController.php
@@ -7,8 +7,8 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function login(){
-        return view('login');
+    public function TampilanWaliSiswa(){
+        return view('TampilanWaliSiswa');
 
     }
 
